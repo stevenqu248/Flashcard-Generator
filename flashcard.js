e7264ca9903db025a19ae18ba4c14fdaf4bfbdd9
Steven Quast <smquast@gmail.com>
2017-05-21 22:15:18 -0500

    updated to show all features

--- flashcard.js
+++ flashcard.js
@@ -61,31 +61,37 @@ ClozeCard.prototype.showPartial = function showPartial()
 	console.log(this.partial);
 }
 
-console.log("This is here to show you the use and functionality of these objects: the BasicCard object and the ClozeCard object.");
-console.log("Feel free to comment or delete these logs when you want to use this code.");
-console.log("Creating a basic flashcard: var basicCard = new BasicCard(\"What is 1 + 1?\",\"2\");");
-var basicCard = new BasicCard("What is 1 + 1?", "2");
+function displayInstructions()
+{
+	console.log("This is here to show you the use and functionality of these objects: the BasicCard object and the ClozeCard object.");
+	console.log("Feel free to comment or delete these logs when you want to use this code.\n\n");
+	console.log("Creating a basic flashcard: var basicCard = new BasicCard(\"What is 1 + 1?\",\"2\");");
+	var basicCard = new BasicCard("What is 1 + 1?", "2");
+	console.log("The first parameter will be what is stored on the front of the card, and the second parameter will be stored on the back.");
+	console.log("Now we will display the data stored in the card to the screen. the parameters you would use are front and back.\n");
+	console.log("basicCard.front contains " + basicCard.front);
+	console.log("basicCard.back contains " + basicCard.back);
+	console.log("\nNow let's move onto the ClozeCard object.");
+	console.log("Creating a Cloze-Deleted flashcard: var clozeCard = new ClozeCard(\"1 + 1 = 2\",\"2\");");
+	var clozeCard = new ClozeCard("1 + 1 = 2", "2");
+	console.log("The first parameter will contain the full text including the part that you want to hide.");
+	console.log("The second parameter will contain what you want removed from the full text.");
+	console.log("Now let's test it out. You can use the parameters text, cloze, and partial");
+	console.log("or you can use the functions showText, showCloze, or showPartial.\n");
+	console.log("clozeCard.text contains " + clozeCard.text);
+	console.log("clozeCard.cloze contains " + clozeCard.cloze);
+	console.log("clozeCard.partial contains " + clozeCard.partial);
+	console.log("clozeCard.showText(); will show:");
+	clozeCard.showText();
+	console.log("clozeCard.showCloze(); will show:");
+	clozeCard.showCloze();
+	console.log("clozeCard.showPartial(); will show:");
+	clozeCard.showPartial();
+	console.log("\nIf you try to create a Cloze-Deleted flashcard improperly, you will receive an error.");
+	console.log("Creating a bad flashcard: var improperCloze = new ClozeCard(\"1 + 1 = 2\",\"Apples, apples, apples!\");");
+	var improperCloze = new ClozeCard("1 + 1 = 2", "Apples, apples, apples");
+	console.log("As you can see, this flashcard is unusable. You will have to try again and create a new, proper flashcard.");
+}
 
-console.log("The first parameter will be what is stored on the front of the card, and the second parameter will be stored on the back.");
-console.log("Now we will display the data stored in the card to the screen. the parameters you would use are front and back.");
-console.log("basicCard.front contains " + basicCard.front);
-console.log("basicCard.back contains " + basicCard.back);
-
-console.log("Now let's move onto the ClozeCard object.");
-console.log("Creating a Cloze-Deleted flashcard: var clozeCard = new ClozeCard(\"1 + 1 = 2\",\"2\");");
-var clozeCard = new ClozeCard("1 + 1 = 2", "2");
-console.log("The first parameter will contain the full text including the part that you want to hide.");
-console.log("The second parameter will contain what you want removed from the full text.");
-console.log("Now let's test it out. You can use the parameters text, cloze, and partial");
-console.log("or you can use the functions showText, showCloze, or showPartial.");
-console.log("clozeCard.text contains " + clozeCard.text);
-console.log("clozeCard.cloze contains " + clozeCard.cloze);
-console.log("clozeCard.partial contains " + clozeCard.partial);
-console.log("clozeCard.showText(); will show:");
-clozeCard.showText();
-console.log("clozeCard.showCloze(); will show:");
-clozeCard.showCloze();
-console.log("clozeCard.showPartial(); will show:");
-clozeCard.showPartial();
-console.log("");
+displayInstructions();
 
